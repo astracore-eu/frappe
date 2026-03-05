bca1f4861911527a1e827d09c3f87ad9e04d63cd
EDSemenov19 <EDSemenov19@codingburgas.bg>
2026-03-05 22:55:54 +0200

    fix: Error connecting to socket.io

--- realtime/utils.js
+++ realtime/utils.js
@@ -5,7 +5,9 @@ function get_url(socket, path) {
 	if (!path) {
 		path = "";
 	}
-	let url = socket.request.headers.origin;
+	let url =
+		socket.request.headers.origin ||
+		`${socket.request.secure ? "https" : "http"}://${socket.request.headers.host}`;
 	if (conf.developer_mode) {
 		let [protocol, host, port] = url.split(":");
 		port = conf.webserver_port;
